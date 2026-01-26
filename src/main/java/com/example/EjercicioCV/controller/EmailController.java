5973550a488378c0024c1da76e27bfbec8546325
Daaviidzz <dballestero366@gmail.com>
2026-01-26 16:22:27 +0100

    gitignore, email controller y Service, dependencias nuevas

---
+++ src/main/java/com/example/EjercicioCV/controller/EmailController.java
@@ -0,0 +1,20 @@
+package com.example.EjercicioCV.controller;
+
+import com.example.EjercicioCV.service.EmailService;
+import org.springframework.web.bind.annotation.*;
+@RestController // Indica que esta clase manejará solicitudes HTTP
+@RequestMapping("/email") // Define la ruta base para las peticiones
+public class EmailController {
+    private final EmailService emailService;
+    public EmailController(final EmailService emailService) {
+        this.emailService = emailService;
+    }
+    @PostMapping("/send") // Define una ruta POST para enviar correos
+    public String enviarCorreo(@RequestParam String to,
+                               @RequestParam String subject,
+                               @RequestParam String body) {
+        emailService.enviarCorreo(to, subject, body);
+        return "Correo enviado correctamente a " + to;
+    }
+}
+
